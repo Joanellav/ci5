a66ca0ee906a502fdeab2f4c2e6728d6bb1298d6
dino <dino@pop-os.localdomain>
2021-12-03 00:58:06 -0500

    plantilla

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import{BrowserRouter, Routes, Route, Link} from "react-router-dom";
+
+import Home from './components/Home';
+import About from './components/About';
+import {Alert, Container} from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Container>
+
+    
+      <Alert variant="secondary" className="mt-1">
+        <Alert.Heading>Hey, nice to see you</Alert.Heading>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aww yeah, you successfully read this important alert message. This example
+          text is going to run a bit longer so that you can see how spacing within an
+          alert works with this kind of content.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Joanella and Brahi
-        </a>
-      </header>
-    </div>
+        <hr />
+        <p className="mb-0">
+          Whenever you need to, be sure to use margin utilities to keep things nice
+          and tidy.
+        </p>
+      </Alert>
+      <BrowserRouter>
+        <nav>
+          <Link to="/" className="btn btn-dark">Home </Link>
+          <Link to="/about" className="btn btn-dark">About</Link>
+        </nav>
+        <hr />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
 
