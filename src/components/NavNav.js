aabefbf2d7167ddaa9a125aae4c8faa3bf742cad
Joanellav <joanellav@uninorte.edu.co>
2021-12-09 21:41:34 -0500

    sign in

--- src/components/NavNav.js
+++ src/components/NavNav.js
@@ -1,54 +1,62 @@
 import{Link} from "react-router-dom";
+// Joa
+import {Button, Modal} from 'react-bootstrap'
+import React, { useState } from 'react';
 
-const NavNav = () => (
+
+
+const NavNav = () => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    return(
 <nav className="navbar navbar-expand-lg navbar-light bg-light ">
 <a className="navbar-brand" href="/">Ciclo 5</a>
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav m-auto">
-          <li className="nav-item active">
-          <Link to="/" className="nav-link">Home<span class="sr-only"></span> </Link>
-          </li>
-          <li className="nav-item">
-          <Link to="/about" className="nav-link">About</Link>
-          </li>
-          <li className="nav-item">
-          <Link to="/contact" className="nav-link">Contact</Link>
-          </li>
-          <li className="nav-item">
-          <Link to="/service" className="nav-link">Service</Link>
-          </li>
-        </ul>			    
-        <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-                <a href="#victorModal" role="button" className="nav-link" data-toggle="modal">Sign in</a>
-                <div id="victorModal" className="modal fade">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <button type="button" className="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                            <h4 className="modal-title">¿Estás seguro?</h4>
-                        </div>
-                        <div className="modal-body">
-                            <p>¿Seguro que quieres borrar este elemento?</p>
-                            <p className="text-warning"><small>Si lo borras, nunca podrás recuperarlo.</small></p>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-default" data-dismiss="modal">Cerrar</button>
-                            <button type="button" className="btn btn-danger">Eliminar</button>
-                        </div>
-                    </div>
-                </div>
-                </div>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/register">Sign up</Link>
-              </li>
-              <li>
-              </li>
-            </ul>
-    </div>
+<ul className="navbar-nav m-auto">
+    <li className="nav-item active">
+        <Link to="/" className="nav-link">Home<span class="sr-only"></span> </Link>
+    </li>
+    <li className="nav-item">
+        <Link to="/about" className="nav-link">About</Link>
+    </li>
+    <li className="nav-item">
+        <Link to="/contact" className="nav-link">Contact</Link>
+    </li>
+    <li className="nav-item">
+        <Link to="/service" className="nav-link">Service</Link>
+    </li>
+</ul>			    
+<ul className="navbar-nav ml-auto">
+    <li>
+        <Button variant="primary" onClick={handleShow}>
+            Launch demo modal
+        </Button>
+    </li>
+
+    <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+    <li className="nav-item">
+        <a href="" role="button" className="nav-link" data-toggle="modal">Sign in</a>
+    </li>
+    <li className="nav-item">
+        <Link className="nav-link" to="/register">Sign up</Link>
+    </li>
+</ul>
 </nav>
 
-);
+ )};
 
 export default NavNav;
